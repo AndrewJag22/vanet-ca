6d29ea48a434d18d741bd91a1c5b86a466e07493
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-12 05:51:19 +0100

    added check if file exists to in close

--- clientcertgenerator.py
+++ clientcertgenerator.py
@@ -12,13 +12,14 @@ class EventHandler(pyinotify.ProcessEvent):
         """                                                                    
         Writtable file was closed.                                              
         """                                                                     
-        if event.pathname.endswith('.csr'):
+        if event.pathname.endswith('.csr') and pathlib.Path(event.pathname).exists():
             fname = event.name.split('.')
             client_csr = incoming + fname[0] + ".csr"
             client_crt = outgoing + fname[0] + ".crt"                                    
             print(fname[0])
             #generates certificate from client's signature request and drops the certificate in sftp/outgoing_certificates folder
             os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)                                                
+
 
     def process_IN_MOVED_TO(self, event):                                       
         """                                                                     
@@ -30,6 +31,8 @@ class EventHandler(pyinotify.ProcessEvent):
             client_crt = outgoing + fname[0] + ".crt"                                      
             print(fname[0])                                            
             os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)
+
+
     def process_IN_CREATE(self, event):                                         
         """                                                                     
         File/dir was created in watched directory.                              
@@ -40,6 +43,7 @@ class EventHandler(pyinotify.ProcessEvent):
             client_crt = outgoing + fname[0] + ".crt" 
             print(fname[0])                                             
             os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)
+
 
 def main():                                                                     
     # watch manager                                                             
