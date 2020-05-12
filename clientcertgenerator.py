13eb6c800e5b426cee00f82c707925902a34b7ec
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-12 05:33:38 +0100

    tweak to delete incoming csr after certification

--- clientcertgenerator.py
+++ clientcertgenerator.py
@@ -18,28 +18,28 @@ class EventHandler(pyinotify.ProcessEvent):
             client_crt = outgoing + fname[0] + ".crt"                                    
             print(fname[0])
             #generates certificate from client's signature request and drops the certificate in sftp/outgoing_certificates folder
-            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_crt)                                                
+            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)                                                
 
     def process_IN_MOVED_TO(self, event):                                       
         """                                                                     
         File/dir was moved to Y in a watched dir (see IN_MOVE_FROM).            
         """                                                                     
-        if event.pathname.endswith('.csr'):                                     
+        if event.pathname.endswith('.csr') and pathlib.Path(event.pathname).exists():                                     
             fname = event.name.split('.')
             client_csr = incoming + fname[0] + ".csr"
             client_crt = outgoing + fname[0] + ".crt"                                      
             print(fname[0])                                            
-            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_crt)
+            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)
     def process_IN_CREATE(self, event):                                         
         """                                                                     
         File/dir was created in watched directory.                              
         """                                                                     
-        if event.pathname.endswith('.csr') and pathlib.Path(client_csr).exists():                                     
+        if event.pathname.endswith('.csr') and pathlib.Path(event.pathname).exists():                                     
             fname = event.name.split('.')                                     
             client_csr = incoming + fname[0] + ".csr"
             client_crt = outgoing + fname[0] + ".crt" 
             print(fname[0])                                             
-            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_crt)
+            os.system("openssl x509 -req -in " + client_csr + " -CA /etc/certs/ca.crt -CAkey /etc/certs/ca.key -CAcreateserial -passin file:/etc/certs/passwordfile -out " + client_crt +" -days 356\nrm " + client_csr)
 
 def main():                                                                     
     # watch manager                                                             
