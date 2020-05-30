cff21e87faf666c3f8902018839db1a2f0359266
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-31 00:35:27 +0100

    minor tweak to file

--- keycertgenerator.sh
+++ keycertgenerator.sh
@@ -10,7 +10,7 @@ sudo mkdir /etc/certs
 echo "password" | sudo tee -a $PASSFILE > /dev/null
 
 #Creates the file containing the Certification Authority info for generating key and certificate
-echo "UK,Manchester,Greater,Manchester,CAserver,server,CA" | sudo tee -a $SUBINFO > /dev/null
+echo "UK,Manchester,Greater Manchester,CAserver,server,CA" | sudo tee -a $SUBINFO > /dev/null
 
 #Loops through created subjectinfo file and assigns each value to a variable
 while IFS="," read -r f1 f2 f3 f4 f5 f6
