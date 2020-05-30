4cf275c85437743170dbea828fa517cf07b7ec99
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-05-30 20:12:23 +0100

    Changed location for logging

--- clientcertgenerator.py
+++ clientcertgenerator.py
@@ -2,7 +2,7 @@ import os, pathlib
 import pyinotify, logging
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s', filename='certificates.log')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s', filename='/var/log/certificates.log')
 
 incoming='/srv/sftp/incoming_requests/'
 outgoing='/srv/sftp/outgoing_certificates/'
