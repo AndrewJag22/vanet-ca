48fcbca5285962891598bab13067640fbaa3fbf4
Temitope Olufohunsi Victor <jimlaps@outlook.com>
2020-06-01 04:54:48 +0100

    added password expect script

--- ca_setup.sh
+++ ca_setup.sh
@@ -1,6 +1,8 @@
 # Creating sftp user profile and sftp folders
-useradd -m vanetclients -p vanetclients
+useradd -m vanetclients
 usermod -s /usr/bin/nologin vanetclients
+chmod +x user_pass.exp
+./user_pass.exp
 groupadd sftpgroup
 usermod -a -G sftpgroup vanetclients
 mkdir /srv/sftp/
@@ -10,11 +12,11 @@ chgrp sftpgroup /srv/sftp/
 chgrp sftpgroup /srv/sftp/incoming_requests
 chgrp sftpgroup /srv/sftp/outgoing_certificates
 chmod 750 /srv/sftp/
-chgrp 770 /srv/sftp/incoming_requests
-chgrp 770 /srv/sftp/outgoing_certificates
+chmod 770 /srv/sftp/incoming_requests
+chmod 770 /srv/sftp/outgoing_certificates
 
 sed -i 's|Subsystem sftp.*|#Subsystem sftp /usr/lib/openssh/sftp-server\nSubsystem sftp internal-sftp|' /etc/ssh/sshd_config
-echo -e "\n#sftp server setup and information\nMatch group sftpgroup\n    ForceCommand internal-sftp\n    X11Forwarding no\n    AllowTcpForwarding no\n    ChrootDirectory /srv/sftp/" >> /etc/ssh/sshd_config
+echo -e "\n#sftp server setup and information\nMatch group sftpgroup\n\tForceCommand internal-sftp\n\tX11Forwarding no\n\tAllowTcpForwarding no\n\tChrootDirectory /srv/sftp/" >> /etc/ssh/sshd_config
 
 # Makes the key generator script executable and then runs it
 chmod +x keycertgenerator.sh
